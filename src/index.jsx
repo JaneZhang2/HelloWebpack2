8acab948ba36cbeeb579a8e8e8da77ea932272de
张静 <jane@bigsec.com>
2017-05-20 14:28:27 +0800

    import style

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './containers/App';
+import './index.scss';
 
 const render = (Component) => {
   ReactDOM.render(
