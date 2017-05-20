9f25daed3c1acc947bca382b5d073c9c85c6b5db
张静 <jane@bigsec.com>
2017-05-20 14:25:53 +0800

    lint

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,9 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class HomeContainer extends Component {
-  render() {
-    return <div>Hello World!</div>;
-  }
-}
+const HomeContainer = () => (
+  <div>Hello World!</div>
+);
 
 export default HomeContainer;
